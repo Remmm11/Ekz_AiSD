b44fc28341681c01d5e792a7a4229e9ba26cd476
Remmm11 <125073822+Remmm11@users.noreply.github.com>
2024-01-15 08:33:36 +0300

    Add files via upload

---
+++ Ekz.py
@@ -0,0 +1,19 @@
+import random as r
+
+sum_str = 0
+min_sum = 10 ** 10
+
+n = int(input('Введите размерность матрицы: '))
+
+matrix = [[r.randint(-10, 10) for i in range(n)] for j in range(n)]
+
+print('Матрица:')
+for i in range(n): print(matrix[i])
+
+for i in range(n):
+    sum_str = sum(matrix[i])
+    if sum_str % 2 != 0 and sum_str < min_sum:
+        min_sum = sum_str
+        index = i
+
+print(f'Строка с минимальной нечётной суммой элементов: {matrix[index][1,]}\nСумма строки: {min_sum}')
